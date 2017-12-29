575324da0a19a0f83bf37a079d4385365b36bb53
Mike Hopper <drmikeh@gmail.com>
2017-12-29 11:35:08 -0500

    Attempting to fix baseurl for gh-pages.

--- src/baseurl.js
+++ src/baseurl.js
@@ -1,4 +1,4 @@
 // GitHub Pages will use a baseUrl of `drmikeh.github.com/d3-examples` so we
 // need to set the basename to include the `/d3-examples` prefix.
 // Otherwise we use an empty string for the basename.
-const baseUrl = window.location.hostname.includes('github') ? '/d3-examples' : '';
+const getBaseUrl = () => window.location.hostname.includes('github') ? '/d3-examples' : '';
